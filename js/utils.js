ffc963548fd7cf6e0bbbe410380fa141f87f04e4
Nathan E. Fairchild <natefair@tellme.com>
2012-06-05 17:03:48 -0700

    Passes JSLint more cleanly.

--- js/utils.js
+++ js/utils.js
@@ -1,44 +1,25 @@
-var month=new Array();
-month[0]="Jan";
-month[1]="Feb";
-month[2]="Mar";
-month[3]="Apr";
-month[4]="May";
-month[5]="Jun";
-month[6]="Jul";
-month[7]="Aug";
-month[8]="Sep";
-month[9]="Oct";
-month[10]="Nov";
-month[11]="Dec";
+var month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 Date.prototype.addDays = function (days) {
     this.setDate(this.getDate() + days);
-}
+};
 
 function isNumber(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
+    return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
-function CurrencyFormatted(amount)
-{
-    var i = parseFloat(amount);
-    if(isNaN(i)) { i = 0.00; }
-    var minus = '';
-    if(i < 0) { minus = '-'; }
-    i = Math.abs(i);
-    i = parseInt((i + .005) * 100);
-    i = i / 100;
-    s = new String(i);
-    if(s.indexOf('.') < 0) { s += '.00'; }
-    if(s.indexOf('.') == (s.length - 2)) { s += '0'; }
-    s = minus + s;
-    return s;
+function CurrencyFormatted(amount) {
+    var i = parseFloat(amount), minus = '', s;
+    if (isNaN(i)) { i = 0.00; }
+    if (i < 0) { minus = '-'; }
+    i = Math.floor((Math.abs(i) + .005) * 100) / 100;
+    s = String(i);
+    if (s.indexOf('.') < 0) { s += '.00'; }
+    if (s.indexOf('.') == (s.length - 2)) { s += '0'; }
+    return minus + s;
 }
 // end of function CurrencyFormatted()
 
-function capitaliseFirstLetter(string)
-{
+function capitaliseFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
